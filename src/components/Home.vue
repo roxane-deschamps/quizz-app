818feeadb0f80f1ba4d0ad2b4739cfcfbe9bbad9
Deschamps <roxane.deschamps@cgi.com>
2018-04-17 00:55:59 +0200

    azudg

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,20 +1,21 @@
 <template>
-    <div class='container'>
-        <div class='row' id='home-presentation'>
-            <h1>REJOIGNEZ L'EXPLORATION DE L'INGÉNIERIE POSITIVE
-            </h1>
+    <div class='container' id='home-container'>
+       
 
-            <h3>LA CLAVETTE EN QUELQUES MOTS</h3>
-                    <p><br>
-            <b>POURQUOI ?</b><br>
-            On considère souvent que l'ingénierie est essentiellement au service de la rentabilité économique. Or il existe une Ingénierie Positive plus humaine qu'on connaît peu et dont on ne parle pas assez.
-            <br><br>
-            <b>COMMENT ?</b><br>
-            En montrant des exemples inspirants d’Ingénierie responsable, innovante et sociale.
-            <br><br>
-            <b>QUOI ?</b><br>
-            Des vidéos et des articles qui présentent et expliquent des initiatives positives à travers le monde.</p>
-        </div>
+            <h1>Quel type d'Ingénieur Positif êtes-vous ?
+            </h1>
+             <div class='row' >
+                 <div class='col-sm-3'></div>
+            <div class='col-sm-6' id='home-description'>
+                <p>Nous souhaitons tous améliorer notre monde mais chacun à notre façon. </p><p>
+                Découvrez les 6 profils qu'on rencontre le plus souvent en Ingénierie Positive.
+                Rassurez-vous, pas besoin d'être ingénieur pour mettre la technologie au service de la société. </p><p>
+                Une manière ludique d'en apprendre plus sur vos moyens d'action.</p>
+                <h4>Prêt à connaître votre profil ?</h4> 
+            </div>
+             </div>
+            
+            
         <div id='home-div-button'>
             <button class='btn btn-primary' @click='beginTest'>Faire le test</button>
         </div>
@@ -33,9 +34,44 @@
 </script>
 
 <style>
-
+#home-container{
+    text-align:center;
+}
 #home-div-button{
     text-align:center;
     margin-top:5%;
+}
+
+#home-div-button .btn-primary{
+    background-color : white;
+    color:rgb(83, 83, 83);;
+    border:none;
+    font-weight:bold;
+    border-top : 0.2em solid  white;
+    border-bottom : 0.3em solid  rgb(134, 206, 0) ;
+    border-radius:0;
+    box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.5);
+    transition: color 0.15s ease-in-out,border-color 0.3s ease-in-out;
+}
+
+#home-div-button .btn-primary:hover{
+    border-bottom : 0.3em solid #5e5e5e;
+
+}
+
+#home-description{
+    margin-top:2rem;
+    background: linear-gradient(to bottom right, #0D47A1,rgb(6, 32, 71));
+    padding :3rem;
+}
+
+#home-description p{
+    font-size:1.2em;
+    text-align: justify;
+}
+
+#home-description h4{
+    font-size:1.2em;
+    font-weight:bolder;
 }
 </style>
--- src/components/Results.vue
+++ src/components/Results.vue
@@ -10,7 +10,10 @@
                         <div class='col col-sm-9' id='results-explanations'>
                             <div class="tab-content" id="v-pills-tabContent">
                             <div class="tab-pane fade" :class="{show : this.indexMaxScore === 0, active: this.indexMaxScore === 0}" id="v-pills-alerteur" role="tabpanel" aria-labelledby="v-pills-alerteur-tab">
-                                <h3>L'alerteur</h3>
+                                <h3>L'alerteur 
+                                    <img class="short" src="../assets/img-profils/alerteur.jpg">
+                                </h3>
+
                                 <h4>Caractéristiques</h4>
                                 <p>Vous n’aimez pas les hypocrites et n’hésitez pas à donner votre avis. En soirée vous pouvez débattre des heures de sujets de société. Vous pensez que nous devons secouer les organisations car le changement, c’est maintenant !</p>
                                 <h4>Super compétence</h4>
@@ -24,7 +27,9 @@
                                 </ul>
                             </div>
                             <div class="tab-pane fade" :class="{show : this.indexMaxScore === 1, active: this.indexMaxScore === 1}" id="v-pills-bricoleur" role="tabpanel" aria-labelledby="v-pills-bricoleur-tab">
-                                <h3>Le bricoleur</h3>
+                                <h3>Le bricoleur
+                                    <img class="short" src="../assets/img-profils/bricoleur.jpg">
+                                </h3>
                                 <h4>Caractéristiques</h4>
                                 <p>Vous êtes passionné par la conception de systèmes. Vos amis ont l’impression que vous êtes toujours en train de bricoler. Parler c’est bien mais ce qui vous intéresse c’est de mettre la main dans le cambouis et de concrétiser les idées.</p>
                                 <h4>Super compétence</h4>
@@ -38,7 +43,9 @@
                                 </ul>
                             </div>
                             <div class="tab-pane fade" :class="{show : this.indexMaxScore === 2, active: this.indexMaxScore === 2}" id="v-pills-collaborateur" role="tabpanel" aria-labelledby="v-pills-collaborateur-tab">
-                                <h3>Le collaborateur</h3>
+                                <h3>Le collaborateur
+                                    <img class="long" src="../assets/img-profils/collaborateur.jpg">
+                                </h3>
                                 <h4>Caractéristiques</h4>
                                 <p>Vous adorez travailler en équipe. Pour vous seul on va peut être plus vite mais à plusieurs on va plus loin. Votre ingéniosité est multipliée quand vous réfléchissez à plusieurs et il vous parait dommage de garder une idée pour soi surtout quand elle a un impact social positif.</p>
                                 <h4>Super compétence</h4>
@@ -53,7 +60,9 @@
                                 </ul>
                             </div>
                             <div class="tab-pane fade" :class="{show : this.indexMaxScore === 3, active: this.indexMaxScore === 3}" id="v-pills-ecolo" role="tabpanel" aria-labelledby="v-pills-ecolo-tab">
-                                <h3>L'écolo</h3>
+                                <h3>L'écolo
+                                    <img class="short" src="../assets/img-profils/ecolo.jpg">
+                                </h3>
                                 <h4>Caractéristiques</h4>
                                 <p>Vous voyez plus loin que le bout de votre nez et la consommation de nos ressources vous inquiète. Vous ne supportez pas qu’on saccage mère Nature. Et vous pensez que la technologie doit se développer dans le respect de la planète et de ses habitants.</p>
                                 <h4>Super compétence</h4>
@@ -67,21 +76,25 @@
                                 </ul>
                             </div>
                             <div class="tab-pane fade" :class="{show : this.indexMaxScore === 4, active: this.indexMaxScore === 4}" id="v-pills-humaniste" role="tabpanel" aria-labelledby="v-pills-humaniste-tab">
-                                <h3>L'humaniste</h3>
+                                <h3>L'humaniste
+                                    <img class="long" src="../assets/img-profils/humaniste.jpg">
+                                </h3>
                                 <h4>Caractéristiques</h4>
                                 <p>Pour vous la technologie doit être au service de l’humanité et un moyen de réduire les inégalités. Vous êtes sensible aux projets qui aident les populations défavorisées car vous pensez que les Hommes sont égaux, peu importent leur sexe, origine ou handicap.</p>
                                 <h4>Super compétence</h4>
                                 <p>Avoir une grande empathie.</p>
                                 <h4>Pistes d'action</h4>
                                 <ul>
-                                    <li>Découvrir l’human center design et l’innovation frugale.</li>
+                                    <li>Découvrir l’<i>human centered design</i> et l’innovation frugale.</li>
                                     <li>Améliorer le bien être de vos coéquipiers.</li>
                                     <li>Aller à la rencontre d’associations ou d’entreprises de l’ESS.</li>
                                     <li>Concevoir des produits qui soient inclusifs.</li>
                                 </ul>
                             </div>
                             <div class="tab-pane fade" :class="{show : this.indexMaxScore === 5, active: this.indexMaxScore === 5}" id="v-pills-pedagogue" role="tabpanel" aria-labelledby="v-pills-pedagogue-tab">
-                                <h3>Le pédagogue</h3>
+                                <h3>Le pédagogue
+                                    <img class="long" src="../assets/img-profils/pedagogue.png">
+                                </h3>
                                 <h4>Caractéristiques</h4>
                                 <p>Vous adorez partager vos savoirs. Vous pensez que la science est un bien commun que nous devons partager. Un petit côté Jamy de C’est pas sorcier mais ce n’est pas grave, vous adoriez l’émission. Il n’est pas rare qu’on vous pose des questions car vous vous faites toujours un plaisir d’y répondre.</p>
                                 <h4>Super compétence</h4>
@@ -196,7 +209,7 @@ import Chart from 'chart.js';
 }
 
 #results-explanations {
-    background-color:rgba(255, 255, 255, 0.932);
+    background-color:rgba(255, 255, 255, 1);
     color:rgb(83, 83, 83);
     padding: 2em !important;
     height:70vh;
@@ -204,7 +217,6 @@ import Chart from 'chart.js';
 }
 
 #results-explanations h4{
-    margin-top:1rem;
     font-size: 1rem;
     font-weight: bold;
     color: rgb(63, 63, 63);
@@ -215,6 +227,7 @@ import Chart from 'chart.js';
     color: rgb(39, 75, 129);
     font-weight: bold;
     font-size:1.6rem;
+    text-align:center;
 }
 #results-explanations p{
     text-align:justify;
@@ -249,6 +262,15 @@ a.nav-link:hover{
     position:absolute;
     bottom:1em;
     right:1em;
+}
+
+#results-explanations img.short{
+    margin-left:0.5em;
+    width:25%;
+}
+
+#results-explanations img.long{
+    width:80%;
 }
 
 </style>
